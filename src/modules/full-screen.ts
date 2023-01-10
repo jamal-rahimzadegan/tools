cb4dac5f12428bfaf44cbd123f78b544eade42e6
Jamal <jamalrahimzadegan@gmail.com>
2023-01-10 12:50:06 +0300

    Create full-screen.ts

---
+++ src/modules/full-screen.ts
@@ -0,0 +1,21 @@
+class FullScreen {
+  private get hasSupport(): boolean {
+    return !!document?.exitFullscreen
+  }
+
+  enter(targetID: string = ''): void {
+    if (!this.hasSupport) return
+
+    const targetElement = document.getElementById(targetID) || document.documentElement
+
+    if (!document.fullscreenElement) targetElement.requestFullscreen()
+  }
+
+  // NOTE: do not call it before entering fullscreen mode.
+  exit(): void {
+    if (!this.hasSupport) return
+    if (document.exitFullscreen) document.exitFullscreen()
+  }
+}
+
+export default new FullScreen()
