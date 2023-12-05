7aa9f35148fd8d5244dd48cdd5af2bb4c9767063
Jamal <jamalrahimzadegan@gmail.com>
2023-12-05 12:07:22 +0000

    Delete src/modules/numbers directory

--- src/modules/numbers/types.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore Todo: fix this
-type ComplexObject<T = string | number | symbol> = Record<T, any>;
-
-type NestedObject = Record<string, any>;
-
-export type { ComplexObject, NestedObject };
