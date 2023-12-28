fdb282be1b761f69abb0c97cc8a17b11cf43c598
Jamal <jamalrahimzadegan@gmail.com>
2023-12-28 14:54:33 +0000

    Update example.tsx

--- src/modules/web-worker/example.tsx
+++ src/modules/web-worker/example.tsx
@@ -2,27 +2,22 @@ import React, { useEffect, useState } from "react";
 import WorkerTool from "./worker-tool";
 const testWorkerFile = require("./test-worker");
 
-export default function Index() {
-	const [mockWorker, setMockWorker] = useState<object>({});
+xport default function Index() {
+  const [mockWorker, setMockWorker] = useState<object>({});
 
-	const handleWorker = () => {
-		let tempWorker: any = new WorkerTool(testWorkerFile);
+  const handleWorker = () => {
+    const tempWorker: any = new WorkerTool(testWorkerFile);
+    tempWorker.onmessage = (e) => console.log(`--- data ----> `, e.data);
+    setMockWorker(tempWorker);
+  };
 
-		tempWorker.onmessage = (e) => {
-			console.log(`--- data ----> `, e.data);
-		};
+  useEffect(handleWorker, []);
 
-		setMockWorker(tempWorker);
-	};
-
-	useEffect(handleWorker, []);
-
-	return (
-		<>
-			<button onClick={() => mockWorker.postMessage(0 ? 100_000 : 50)}>
-				Run a heavy task
-			</button>
-			<p>Can you click and select me without problem?</p>
-		</>
-	);
+  return (
+    <>
+      <button onClick={() => mockWorker.postMessage(0 ? 100_000 : 50)}>Run a heavy task</button>
+      <p>Can you click and select me without problem?</p>
+    </>
+  );
 }
+
